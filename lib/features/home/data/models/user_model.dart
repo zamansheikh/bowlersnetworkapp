623e428f91cd018938d708206514719ef89d8105
Zaman Sheikh <zaman6545@gmail.com>
2025-09-09 11:22:32 +0600

    Refactor code structure for improved readability and maintainability

--- lib/features/home/data/models/user_model.dart
+++ lib/features/home/data/models/user_model.dart
@@ -5,14 +5,91 @@ part 'user_model.g.dart';
 
 @JsonSerializable()
 class UserModel extends User {
+  final String username;
+  final String firstName;
+  final String lastName;
+  final String profilePictureUrl;
+  final String introVideoUrl;
+  final String coverPhotoUrl;
+  final int xp;
+  final int level;
+  final String cardTheme;
+  final bool isPro;
+  final List<BrandModel> sponsors;
+  final int followerCount;
+  final StatsModel stats;
+  final List<BrandModel> favoriteBrands;
+  final bool isComplete;
+
   const UserModel({
     required super.id,
     required super.name,
     required super.email,
+    required this.username,
+    required this.firstName,
+    required this.lastName,
+    required this.profilePictureUrl,
+    required this.introVideoUrl,
+    required this.coverPhotoUrl,
+    required this.xp,
+    required this.level,
+    required this.cardTheme,
+    required this.isPro,
+    required this.sponsors,
+    required this.followerCount,
+    required this.stats,
+    required this.favoriteBrands,
+    required this.isComplete,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) =>
       _$UserModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$UserModelToJson(this);
+}
+
+@JsonSerializable()
+class BrandModel {
+  final int brandId;
+  final String brandType;
+  final String name;
+  final String formalName;
+  final String logoUrl;
+
+  const BrandModel({
+    required this.brandId,
+    required this.brandType,
+    required this.name,
+    required this.formalName,
+    required this.logoUrl,
+  });
+
+  factory BrandModel.fromJson(Map<String, dynamic> json) =>
+      _$BrandModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BrandModelToJson(this);
+}
+
+@JsonSerializable()
+class StatsModel {
+  final int id;
+  final int userId;
+  final double averageScore;
+  final int highGame;
+  final int highSeries;
+  final int experience;
+
+  const StatsModel({
+    required this.id,
+    required this.userId,
+    required this.averageScore,
+    required this.highGame,
+    required this.highSeries,
+    required this.experience,
+  });
+
+  factory StatsModel.fromJson(Map<String, dynamic> json) =>
+      _$StatsModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$StatsModelToJson(this);
 }
--- lib/features/home/data/models/user_model.g.dart
+++ lib/features/home/data/models/user_model.g.dart
@@ -10,10 +10,80 @@ UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
   id: json['id'] as String,
   name: json['name'] as String,
   email: json['email'] as String,
+  username: json['username'] as String,
+  firstName: json['firstName'] as String,
+  lastName: json['lastName'] as String,
+  profilePictureUrl: json['profilePictureUrl'] as String,
+  introVideoUrl: json['introVideoUrl'] as String,
+  coverPhotoUrl: json['coverPhotoUrl'] as String,
+  xp: (json['xp'] as num).toInt(),
+  level: (json['level'] as num).toInt(),
+  cardTheme: json['cardTheme'] as String,
+  isPro: json['isPro'] as bool,
+  sponsors: (json['sponsors'] as List<dynamic>)
+      .map((e) => BrandModel.fromJson(e as Map<String, dynamic>))
+      .toList(),
+  followerCount: (json['followerCount'] as num).toInt(),
+  stats: StatsModel.fromJson(json['stats'] as Map<String, dynamic>),
+  favoriteBrands: (json['favoriteBrands'] as List<dynamic>)
+      .map((e) => BrandModel.fromJson(e as Map<String, dynamic>))
+      .toList(),
+  isComplete: json['isComplete'] as bool,
 );
 
 Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
   'id': instance.id,
   'name': instance.name,
   'email': instance.email,
+  'username': instance.username,
+  'firstName': instance.firstName,
+  'lastName': instance.lastName,
+  'profilePictureUrl': instance.profilePictureUrl,
+  'introVideoUrl': instance.introVideoUrl,
+  'coverPhotoUrl': instance.coverPhotoUrl,
+  'xp': instance.xp,
+  'level': instance.level,
+  'cardTheme': instance.cardTheme,
+  'isPro': instance.isPro,
+  'sponsors': instance.sponsors,
+  'followerCount': instance.followerCount,
+  'stats': instance.stats,
+  'favoriteBrands': instance.favoriteBrands,
+  'isComplete': instance.isComplete,
 };
+
+BrandModel _$BrandModelFromJson(Map<String, dynamic> json) => BrandModel(
+  brandId: (json['brandId'] as num).toInt(),
+  brandType: json['brandType'] as String,
+  name: json['name'] as String,
+  formalName: json['formalName'] as String,
+  logoUrl: json['logoUrl'] as String,
+);
+
+Map<String, dynamic> _$BrandModelToJson(BrandModel instance) =>
+    <String, dynamic>{
+      'brandId': instance.brandId,
+      'brandType': instance.brandType,
+      'name': instance.name,
+      'formalName': instance.formalName,
+      'logoUrl': instance.logoUrl,
+    };
+
+StatsModel _$StatsModelFromJson(Map<String, dynamic> json) => StatsModel(
+  id: (json['id'] as num).toInt(),
+  userId: (json['userId'] as num).toInt(),
+  averageScore: (json['averageScore'] as num).toDouble(),
+  highGame: (json['highGame'] as num).toInt(),
+  highSeries: (json['highSeries'] as num).toInt(),
+  experience: (json['experience'] as num).toInt(),
+);
+
+Map<String, dynamic> _$StatsModelToJson(StatsModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'userId': instance.userId,
+      'averageScore': instance.averageScore,
+      'highGame': instance.highGame,
+      'highSeries': instance.highSeries,
+      'experience': instance.experience,
+    };
