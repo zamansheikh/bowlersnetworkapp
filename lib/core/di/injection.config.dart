7f46453f5334f8a8055c24d1f24a82ce498d5fa0
Zaman Sheikh <zaman6545@gmail.com>
2025-09-09 10:04:47 +0600

    Refactor dependency injection configuration and streamline imports

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -9,6 +9,19 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:bowlersnetworkapp/core/network/network_info.dart' as _i149;
+import 'package:bowlersnetworkapp/core/network/network_module.dart' as _i516;
+import 'package:bowlersnetworkapp/features/home/data/datasources/user_remote_data_source.dart'
+    as _i528;
+import 'package:bowlersnetworkapp/features/home/data/repositories/user_repository_impl.dart'
+    as _i224;
+import 'package:bowlersnetworkapp/features/home/domain/repositories/user_repository.dart'
+    as _i637;
+import 'package:bowlersnetworkapp/features/home/domain/usecases/get_users.dart'
+    as _i450;
+import 'package:bowlersnetworkapp/features/home/presentation/bloc/home_bloc.dart'
+    as _i229;
+import 'package:dio/dio.dart' as _i361;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
@@ -18,7 +31,24 @@ extension GetItInjectableX on _i174.GetIt {
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    _i526.GetItHelper(this, environment, environmentFilter);
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    final networkModule = _$NetworkModule();
+    gh.factory<_i229.HomeBloc>(() => _i229.HomeBloc());
+    gh.lazySingleton<_i361.Dio>(() => networkModule.dio);
+    gh.lazySingleton<_i528.UserRemoteDataSource>(
+      () => _i528.UserRemoteDataSourceImpl(),
+    );
+    gh.lazySingleton<_i149.NetworkInfo>(() => _i149.NetworkInfoImpl());
+    gh.lazySingleton<_i637.UserRepository>(
+      () => _i224.UserRepositoryImpl(
+        remoteDataSource: gh<_i528.UserRemoteDataSource>(),
+      ),
+    );
+    gh.factory<_i450.GetUsers>(
+      () => _i450.GetUsers(gh<_i637.UserRepository>()),
+    );
     return this;
   }
 }
+
+class _$NetworkModule extends _i516.NetworkModule {}
--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,14 +1,5 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-
-// Import all the modules and classes that need to be registered
-import '../network/network_module.dart';
-import '../network/network_info.dart';
-import '../../features/home/presentation/bloc/home_bloc.dart';
-import '../../features/home/domain/usecases/get_users.dart';
-import '../../features/home/data/repositories/user_repository_impl.dart';
-import '../../features/home/data/datasources/user_remote_data_source.dart';
-
 import 'injection.config.dart';
 
 final getIt = GetIt.instance;
