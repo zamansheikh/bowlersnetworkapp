e615a5ed017a972636e87393f7becc6ec9e77da9
Zaman Sheikh <zaman6545@gmail.com>
2025-09-16 23:42:43 +0600

    refactor: Replace hardcoded gradient with AppColors.primaryGradient in UserAccountsDrawerHeader

--- lib/core/widgets/app_drawer.dart
+++ lib/core/widgets/app_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:bowlersnetworkapp/core/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -51,11 +52,7 @@ class AppDrawer extends StatelessWidget {
                       }
                       return UserAccountsDrawerHeader(
                         decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [Colors.blue, Colors.blue.shade800],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                          ),
+                          gradient: AppColors.primaryGradient,
                         ),
                         accountName: Text(
                           user.name,
